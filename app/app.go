40bb8da7cdd2d4cdf62d276beff82a5a5e384042
misua <charlespogzz@googlemail.com>
2022-05-29 14:23:49 +0800

    commit using goland ui

--- app/app.go
+++ app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"banking/domain"
+	"banking/service"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -11,11 +13,14 @@ func Start() {
 	//mux := http.NewServeMux()
 	router := mux.NewRouter()
 
-	//define routes
+	//wiring
+	ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
 
-	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
-	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
-	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+	//define routes
+	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
+
+	//router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	//router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
--- app/handlers.go
+++ app/handlers.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"banking/service"
 	"encoding/json"
 	"encoding/xml"
 	fmt "fmt"
@@ -14,17 +15,18 @@ type Customer struct {
 	Zipcode string `json:"zip_code,omitempty"`
 }
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello world!!")
-
+type CustomerHandlers struct {
+	service service.CustomerService
 }
 
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{"Charles", "Davao City", "9000"},
-		{"sab", "Davao City", "8000"},
-		{"chaz", "Davao City", "7000"},
-	}
+func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	//customers := []Customer{
+	//	{"Charles", "Davao City", "9000"},
+	//	{"sab", "Davao City", "8000"},
+	//	{"chaz", "Davao City", "7000"},
+	//}
+
+	customers, _ := ch.service.GetAllCustomer()
 
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Add("Content-type", "application/xml")
